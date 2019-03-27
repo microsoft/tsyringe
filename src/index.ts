b28807c804c49d86ff0f64a5b61f0cd156a5f9c2
Alexey Prokhorov <megahertz@l96.ru>
2019-03-28 01:59:21 +0400

    Add a runtime check of reflect polyfill (#18) (#24)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import {DependencyContainer} from "./types";
 import {instance} from "./dependency-container";
 
+if (typeof Reflect === "undefined" || !Reflect.getMetadata) {
+  throw `tsyringe requires a reflect polyfill. Please add 'import "reflect-metadata"' to the top of your entry point.`;
+}
+
 export {DependencyContainer} from "./types";
 export * from "./decorators";
 export * from "./factories";
