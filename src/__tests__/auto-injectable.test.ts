48205503c1c4246925766c9a54cce4843811ee08
Thiago Oliveira <thiago_ogt@outlook.com>
2019-06-12 02:58:36 -0300

    @autoInjectable resolve multiple dependencies * Change dependency resolution on decorator @autoInjectable * Add new test cases

--- src/__tests__/auto-injectable.test.ts
+++ src/__tests__/auto-injectable.test.ts
@@ -1,5 +1,6 @@
 import {autoInjectable, injectable, singleton} from "../decorators";
 import {instance as globalContainer} from "../dependency-container";
+import injectAll from "../decorators/inject-all";
 
 afterEach(() => {
   globalContainer.reset();
@@ -142,14 +143,39 @@ test("@autoInjectable works with @singleton", () => {
   expect(instance1.bar).toBe(instance2.bar);
 });
 
-test("@autoInjectable throws for array dependency", () => {
+test("@autoInjectable resolves multiple registered dependencies", () => {
+  interface Bar {
+    str: string;
+  }
+
   @injectable()
-  class Bar {}
+  class FooBar implements Bar {
+    str: string = "";
+  }
+
+  globalContainer.register<Bar>("Bar", {useClass: FooBar});
 
   @autoInjectable()
   class Foo {
-    constructor(public bar?: Bar[]) {}
+    constructor(@injectAll("Bar") public bar?: Bar[]) {}
   }
 
-  expect(() => new Foo()).toThrow();
+  const foo = new Foo();
+  expect(Array.isArray(foo.bar)).toBeTruthy();
+  expect(foo.bar!.length).toBe(1);
+  expect(foo.bar![0]).toBeInstanceOf(FooBar);
+});
+
+test("@autoInjectable resolves multiple transient dependencies", () => {
+  class Foo {}
+
+  @autoInjectable()
+  class Bar {
+    constructor(@injectAll(Foo) public foo?: Foo[]) {}
+  }
+
+  const bar = new Bar();
+  expect(Array.isArray(bar.foo)).toBeTruthy();
+  expect(bar.foo!.length).toBe(1);
+  expect(bar.foo![0]).toBeInstanceOf(Foo);
 });
