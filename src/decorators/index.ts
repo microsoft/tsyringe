cc1a0541102a398bebdca17f737f2fb0c5bdd3bb
Sergey Sokolov <ssokolov2@alfabank.ru>
2019-08-05 14:58:18 +0300

    Fixed missing injectAll reexport

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -3,3 +3,4 @@ export {default as inject} from "./inject";
 export {default as injectable} from "./injectable";
 export {default as registry} from "./registry";
 export {default as singleton} from "./singleton";
+export {default as injectAll} from "./inject-all";
