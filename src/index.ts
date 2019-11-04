4672283445552265750408bd53d60918bb603237
Thiago Oliveira <thiago_ogt@outlook.com>
2019-11-04 11:04:16 -0300

    export unexported types

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ if (typeof Reflect === "undefined" || !Reflect.getMetadata) {
   throw `tsyringe requires a reflect polyfill. Please add 'import "reflect-metadata"' to the top of your entry point.`;
 }
 
-export {DependencyContainer} from "./types";
+export {DependencyContainer, Lifecycle, RegistrationOptions} from "./types";
 export * from "./decorators";
 export * from "./factories";
 export * from "./providers";
