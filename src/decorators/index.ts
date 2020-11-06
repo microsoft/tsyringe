33ac46853f1c1e9e0d07b67485ed936bf43b900d
Lee Chang <leechang@microsoft.com>
2020-11-06 14:05:21 -0800

    Export new decorators

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -4,4 +4,6 @@ export {default as injectable} from "./injectable";
 export {default as registry} from "./registry";
 export {default as singleton} from "./singleton";
 export {default as injectAll} from "./inject-all";
+export {default as injectAllWithTransform} from "./inject-all-with-transform";
+export {default as injectWithTransform} from "./inject-with-transform";
 export {default as scoped} from "./scoped";
