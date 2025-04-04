4f21f15d4612dd046b4b4fe47a42629f05a7d545
Etienne Noel <etiennenoel@gmail.com>
2025-04-03 20:29:36 -0700

    Adds the ability to register additional tokens via the @injectable decorator.

--- src/decorators/injectable.ts
+++ src/decorators/injectable.ts
@@ -1,6 +1,8 @@
 import constructor from "../types/constructor";
 import {getParamInfo} from "../reflection-helpers";
 import {typeInfo} from "../dependency-container";
+import InjectionToken from "../providers/injection-token";
+import {instance as globalContainer} from "../dependency-container";
 
 /**
  * Class decorator factory that allows the class' dependencies to be injected
@@ -8,9 +10,21 @@ import {typeInfo} from "../dependency-container";
  *
  * @return {Function} The class decorator
  */
-function injectable<T>(): (target: constructor<T>) => void {
+function injectable<T>(options?: {
+  token?: InjectionToken<T> | InjectionToken<T>[];
+}): (target: constructor<T>) => void {
   return function(target: constructor<T>): void {
     typeInfo.set(target, getParamInfo(target));
+
+    if (options && options.token) {
+      if (!Array.isArray(options.token)) {
+        globalContainer.register(options.token, target);
+      } else {
+        options.token.forEach(token => {
+          globalContainer.register(token, target);
+        });
+      }
+    }
   };
 }
 
